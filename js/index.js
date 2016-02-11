ecc38bc9aea767f5e833c8d3891f4256e298917a
jayrenn <jayrenn@users.noreply.github.com>
2016-02-11 11:00:23 -0800

    nit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
     var mediaStreamType = Windows.Media.Capture.MediaStreamType.videoRecord;
     var mediaCaptureInitializationSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings;
     var displayRequest = new Windows.System.Display.DisplayRequest();
-    var faceboxColors = ["#e74c3c", "#3498db", "#2ecc71", "#f1c40f", "#9b59b6", "#e67e22"]
+    var faceboxColors = ["#e74c3c", "#3498db", "#2ecc71", "#f1c40f", "#9b59b6", "#e67e22"];
 
     function createFacebox(face, num) {
         var facebox = document.createElement("div");
